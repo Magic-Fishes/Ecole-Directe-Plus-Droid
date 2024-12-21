5eb57c707b0ef542e2c06e7ab49cee2e7643d1e0
Ewalwi <141340241+Ewalwi@users.noreply.github.com>
2024-12-21 22:48:49 +0100

    Modified dev recertification embed

--- commands/devCertif/devreevalping.js
+++ commands/devCertif/devreevalping.js
@@ -7,11 +7,12 @@ module.exports = {
     description: "ping every dev on dev general to announce a recertification",
     run: async (client, interaction) => {
         const devRole = interaction.guild.roles.cache.find(role => role.name === 'Développeur');
+        const active_dev_role = interaction.guild.roles.cache.find(role => role.id === 1280907843238760451);
         const devChannel = interaction.guild.channels.cache.find(channel => channel.name === 'général-dev');
 
         const embedData = JSON.parse(fs.readFileSync(path.join(__dirname, '../../utils/embeds/dev_reeval_ping.json'), 'utf8'));
 
-        const description = embedData.description.replace("{dev_role}", devRole);
+        const description = embedData.description.replace("{dev_role}", devRole).replace("{active_dev_role}", active_dev_role);
 
         const announcementEmbed = new EmbedBuilder()
             .setTitle(embedData.title)
