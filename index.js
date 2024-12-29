eb8378acbfa538b8033765e7c4229880433404de
Ewalwi <141340241+Ewalwi@users.noreply.github.com>
2024-12-29 10:56:27 +0100

    updated index.js

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 const Discord = require("discord.js");
-const eventHandler = require("./handlers/eventHandler");
 require("dotenv").config();
-const client = new Discord.Client({ intents: 3276799 });
 
-eventHandler(client);
+const Client = new Discord.Client({ partials: ["CHANNEL"], intents: 19992575 });
 
-client.login(process.env.TOKEN);
+// Error handling 
+
+process.on("exit", code => {console.log("Le processus s'est arrêté avec le code : " + code) });
+process.on("uncaughtException", (err, origin) => { console.log("[UNCAUGHT_EXCEPTION] : " + err, "Origine : " + origin)});
+process.on("unhandledRejection", (reason, promise) => { console.log("[UNHANDLED_REJECTION] : " + reason, "Promise : " + promise)});
+process.on("warning", (...args) => { console.log("[WARNING] :", ...args)});
+
+Client.on("error", console.error);
+Client.on("warn", console.warn);
+
+["commands", "cooldowns", "buttons", "selects"].forEach(x => Client[x] = new Discord.Collection());
+require(`./src/utils/handlers/CommandUtil`)(Client)
+require(`./src/utils/handlers/EventUtil`)(Client)
+require(`./src/utils/handlers/ButtonUtil`)(Client)
+require(`./src/utils/handlers/SelectUtil`)(Client)
+
+Client.login(process.env.TOKEN);
+console.log(`BOT Running in ${process.env.NODE_ENV}`)
