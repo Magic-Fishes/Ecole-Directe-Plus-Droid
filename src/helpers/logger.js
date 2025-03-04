72e699f975e7da98b4c2ee56f27235eae8913759
as2pick <vooxify.public@outlook.fr>
2025-03-04 13:17:58 +0100

    edit logger

--- src/helpers/logger.js
+++ src/helpers/logger.js
@@ -1,11 +1,18 @@
 const { createLogger, format, transports } = require("winston");
+const DailyRotateFile = require("winston-daily-rotate-file");
 
 const logger = createLogger({
     level: "info",
     format: format.combine(format.timestamp(), format.json()),
     transports: [
         new transports.Console(),
-        new transports.File({ filename: "combined.log" }),
+        new DailyRotateFile({
+            filename: "application-%DATE%.log",
+            datePattern: "YYYY-MM-DD-HH",
+            zippedArchive: true,
+            maxSize: "20m",
+            maxFiles: "14d",
+        }),
     ],
 });
 
